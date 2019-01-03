062a36cab2f86670937ccdd85c80fa6a294a1eec
maxCut <maxCut@github.com>
2019-01-03 15:42:48 -0500

    first commit

---
+++ WorldUI.java
@@ -0,0 +1,10 @@
+/*
+ *  This class is a graphical interface used to interact with a blob world.
+ */
+public class WorldUI
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+    }
+}
